cca8195542526f30326bea7d9a230d892bcd1427
Efelrine <efelrine@gmail.com>
2014-09-20 00:40:22 +0200

    Fix Bootstrap

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name,:number)
+      params.require(:item).permit(:name,:number, :owner_id)
     end
 end
